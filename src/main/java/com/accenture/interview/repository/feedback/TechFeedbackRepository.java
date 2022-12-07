a8f80b67ea9e45430e2f342402bfd72e6918490d
daniele.consorte <daniele.consorte@C11-I23DF2XV4CL.dir.svc.accenture.com>
2022-12-07 14:34:23 +0100

    [#23] Refactor TechnicalFeedbackRTO + correttiva eliminazione score tech feedback form

--- src/main/java/com/accenture/interview/repository/feedback/TechFeedbackRepository.java
+++ src/main/java/com/accenture/interview/repository/feedback/TechFeedbackRepository.java
@@ -24,13 +24,14 @@ public interface TechFeedbackRepository extends JpaRepository<TechnicalFeedback,
 	 */
 	@Query("select tf from Tech_feedback tf where tf.interview.id=:idColloquio")
 	Optional<TechnicalFeedback> getTechFeedbackByIdInterview(@Param("idColloquio") Long idColloquio);
-
+	
 	/**
 	 * Gets the tech feedback RTO by id interview.
 	 *
 	 * @param idColloquio the id colloquio
 	 * @return the tech feedback RTO by id interview
 	 */
-	@Query("select tf from Tech_feedback tf where tf.interview.id=:idColloquio")
-	TechnicalFeedbackRTO getTechFeedbackRTOByIdInterview(@Param("idColloquio") Long idColloquio);
+	@Query("select new com.accenture.interview.rto.feedback.TechnicalFeedbackRTO(i.id, i.comment, i.scores)" +
+			" from Tech_feedback i where i.interview.id=:idColloquio")
+	TechnicalFeedbackRTO getTechFeedbackRTOByIdInterview(@Param("idColloquio") Long idColloquio);	
 }
